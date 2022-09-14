c4623a2489a4ecf397d1257f272bbb1e8b813f54
gusradika <aditezo24@gmail.com>
2022-09-14 09:40:50 +0700

    Setter and Getter

---
+++ object1/hewan.java
@@ -0,0 +1,27 @@
+package object1;
+
+public class hewan {
+    public hewan() {
+        // attributes
+
+    }
+
+    private String tipe, suara;
+    private int usia;
+
+    // setter dari semua variable attribute/data
+    // setter selalu memiliki parameter
+    public void setTipe(String t) {
+        tipe = t;
+    }
+
+    public void setSuara(String suara) {
+        this.suara = suara;
+    }
+
+    public void setUsia(int usia) {
+        this.usia = usia;
+    }
+    // getter dari semua variable attribute/data
+
+}
